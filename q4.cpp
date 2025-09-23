6e6e388cb4aced6130533ca12fb7b2d113fcb9b5
r4coder <ritheeshk2003@gmail.com>
2025-09-23 20:17:49 +0530

    Create q4.cpp

---
+++ q4.cpp
@@ -0,0 +1,57 @@
+class Solution {
+  public:
+  
+  int frog(int n,vector<int>&dp,vector<int>&height,int k)
+  {
+      
+      if(n==0)
+      {
+          return 0;
+      }
+
+      
+      
+       if(dp[n]!=-1)
+       {
+           
+           return dp[n];
+       }
+       int mini=INT_MAX;
+       
+       
+       for(int i=n-1;i<=max(0,n-k);i--)
+       {
+           dp[i]=frog(i,dp,height,k)+abs(height[i]-height[i-1]);
+           mini=min(mini,dp[i]);
+           
+           
+           
+           
+           
+       }
+       
+       return dp[n]=mini;
+       
+        
+      
+  }
+  
+  
+  
+  
+    int minCost(vector<int>& height) {
+        
+        int n=height.size()-1;
+        vector<int>dp(n+1,-1)
+        return frog(n,dp,height,k);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+};
